1eca740bfc1e969fc75296d0d348b88a2f69edb8
Lazzzycatwastaken <tomekgorecki269@gmail.com>
2025-05-09 19:38:11 +0200

    whos getting the best head: A. Alvin B. Theodore C. Simon (Secret option) Me

--- src/main/java/com/hbm/world/gen/NTMWorldGenerator.java
+++ src/main/java/com/hbm/world/gen/NTMWorldGenerator.java
@@ -39,7 +39,6 @@ public class NTMWorldGenerator implements IWorldGenerator {
 		final List<BiomeGenBase> oceanBiomes = Arrays.asList(new BiomeGenBase[] { BiomeGenBase.ocean, BiomeGenBase.deepOcean });
 		final List<BiomeGenBase> beachBiomes = Arrays.asList(new BiomeGenBase[] { BiomeGenBase.beach, BiomeGenBase.stoneBeach, BiomeGenBase.coldBeach });
 		final List<BiomeGenBase> lighthouseBiomes = Arrays.asList(new BiomeGenBase[] { BiomeGenBase.ocean, BiomeGenBase.deepOcean, BiomeGenBase.beach, BiomeGenBase.stoneBeach, BiomeGenBase.coldBeach });
-		final List<BiomeGenBase> flatbiomes = Arrays.asList(new BiomeGenBase[] { BiomeGenBase.plains, BiomeGenBase.icePlains, BiomeGenBase.desert });
 
 		/// SPIRE ///
 		NBTStructure.registerStructure(0, new SpawnCondition() {{
@@ -103,21 +102,18 @@ public class NTMWorldGenerator implements IWorldGenerator {
 			spawnWeight = 8;
 		}});
 
+		NBTStructure.registerNullWeight(0, 2, oceanBiomes::contains); //why the fuck did this change
+
 		NBTStructure.registerStructure(0, new SpawnCondition() {{
-			canSpawn = flatbiomes::contains;
+			canSpawn = biome -> biome == BiomeGenBase.plains;
 			structure = new JigsawPiece("dish", StructureManager.dish, -10);
 			minHeight = 53;
 			maxHeight = 65;
-			spawnWeight = 4;
+			spawnWeight = 1;
 		}});
 
-		NBTStructure.registerStructure(0, new SpawnCondition() {{
-			canSpawn = flatbiomes::contains;
-			structure = new JigsawPiece("factory", StructureManager.factory, -10);
-			spawnWeight = 46;
-		}});
-
-		NBTStructure.registerNullWeight(0, 4, oceanBiomes::contains);
+		NBTStructure.registerNullWeight(0, 2, biome -> biome == BiomeGenBase.plains);
+		NBTStructure.registerNullWeight(0, 2, oceanBiomes::contains);
 
 		Map<Block, BlockSelector> bricks = new HashMap<Block, BlockSelector>() {{
 			put(ModBlocks.meteor_brick, new MeteorBricks());
